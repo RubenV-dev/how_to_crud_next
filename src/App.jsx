613c8f2b5b3b0f38b8cc83a9ec37f84be08678af
Ruben Vallejo <r.will16@hotmail.com>
2025-09-17 14:13:10 -0400

    Project Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ModelForm from './components/ModelForm'
+import NavBar from './components/Navbar'
+import TableList from './components/Tablelist'
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <TableList/>
+      <ModelForm/>
+    </>
+  )
+}
+
+export default App
